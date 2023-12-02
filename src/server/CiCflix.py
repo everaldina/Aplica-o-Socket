60be6f89a4d400ffca810f17de329c2700e345fc
Everaldina G. Barbosa <everaldinag@gmail.com>
2023-12-02 20:44:44 -0300

    add arquivo CiCflix

---
+++ src/server/CiCflix.py
@@ -0,0 +1,41 @@
+import json
+import os
+
+def searchYear(index, search):
+    list = []
+    for i in index:
+        if "premiered" in i:
+            year = i["premiered"].split("-")[0]
+            if search == year:
+                list.append(i)
+        else:
+            if search == str(i["year"]):
+                list.append(i)
+            
+searchType = lambda x, search: [i for i in x if i["type"] == search]
+searchDirector = lambda x, search: [i for i in x if search in i["director"] or search in i["creator"]]
+searchTitle = lambda x, search: [i for i in x if search in i["original_title"] or search in i["title_ptBR"]]
+searchGenre = lambda x, search: [i for i in x if search in i["genres"]]
+
+def searchFile(search, type):
+    current_dir = os.path.dirname(os.path.abspath(__file__))
+    file_path = os.path.join(current_dir, "data/index.json")
+    list = []
+    
+    with open(file_path) as json_file:
+        list = json.load(json_file)
+        
+    if type == "titulo":
+        results = searchTitle(list, search)
+    elif type == "genero":
+        results = searchGenre(list, search)
+    elif type == "ano":
+        results = searchYear(list, search)
+    elif type == "diretor":
+        results = searchDirector(list, search)
+    elif type == "tipo": 
+        results = searchType(list, search)
+        
+    return results
+    
+    
