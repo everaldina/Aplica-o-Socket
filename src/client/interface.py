08f770d1bb24bc9f40058630d00629af7e7e1c35
Everaldina G. Barbosa <everaldinag@gmail.com>
2023-12-04 18:47:06 -0300

    add comentarios interface

--- src/client/interface.py
+++ src/client/interface.py
@@ -1,11 +1,15 @@
 import os
 from client import searchFile, streamFile, closeServer
-import json
 
 def main():
-    # menu principal
+    '''
+        Função principal da interface do usuario, é responsavel por 
+        chamar as funções de busca e stream.
+    '''
+    
     while(1):
         logo_grande()
+        # menu principal
         print("\t\t1. Busca por titulo")
         print("\t\t2. Busca por genero")
         print("\t\t3. Busca por ano")
@@ -13,19 +17,22 @@ def main():
         print("\t\t5. Busca por tipo")
         print("\t\t0. Sair")
         opc = input("Busca por: ")
-            
+        
+        # busca vai receber o termo de busca que o usuario digitou
         busca = menu(opc)
         
         # --- é usado para separar os dados enviados ao servidor e nao pode ser usado como busca
         if busca is not None and "---" in busca:
             print("Cadeia de caracterer '---' invalido")
             input()
-            #limpar_terminal()
+            limpar_terminal()
             continue
         
+        # caso o usuario tenha digitado algo invalido busca vai ser None
         if busca is None:
             continue
-            
+        
+        # manda o tipo de busca para o servidor de acordo com a opção escolhida
         match opc:
             case "1":
                 procura_por = "titulo"
@@ -37,70 +44,101 @@ def main():
                 procura_por = "diretor"
             case "5":
                 procura_por = "tipo"
-        list = searchFile(busca, procura_por)
+                
+        # sevidor deve mandar uma lista com os resultados da busca
+        # caso haja um erro na conexão com o servidor um erro é levantado
+        try:
+            list = searchFile(busca, procura_por)
+        except Exception as e:
+            print(str(e))
         
-        #limpar_terminal()
+        # imprime os resultados da busca
+        limpar_terminal()
         logo_pequena()
         print(f"-------Resultados-------")
-        if len(list) == 0:
+        
+        
+        if len(list) == 0: # se a lista estiver vazia
             print("Nenhum resultado encontrado")
             input()
-            #limpar_terminal()
+            limpar_terminal()
             continue
-        else:
+        else: 
+            # print_media imprime os resultados da busca e retorna uma lista com os ids
             ids = print_media(list)
+            
+            # o usuario pode escolher um id para assistir ou voltar ao menu principal
             id_selected = input("Selecione o id para assistir ou 0 para voltar: ")
-            if id_selected == "0":
+            
+            if id_selected == "0": # se o usuario escolher voltar
                 continue
-            if id_selected not in ids:
+            if id_selected not in ids: # caso o usuario digite um id invalido
                 print("Opcao invalida")
                 input()
-                #limpar_terminal()
+                limpar_terminal()
                 continue
-            else:
-                #limpar_terminal()
+            else: # se o usuario escolher um id valido
+                limpar_terminal()
+                
+                # procura o id selecionado na lista de resultados
                 for i in list:
                     if str(i["id"]) == id_selected:
                         selected = i
                         break
+                
                 print(f"Assistindo {selected['title_ptBR']}")
-                streamFile(id_selected)
-                print("Fim.......")
-                input("Pressione enter para voltar ao menu principal......")
-                #limpar_terminal()
+                
+                # tenta fazer o stream do arquivo, caso haja um erro uma mensagem é impressa
+                try:
+                    streamFile(id_selected) # faz o stream do arquivo
+                    print("Fim.......")
+                    input("Pressione enter para voltar ao menu principal......")
+                except Exception as e:
+                    print(str(e))
+                    input()
+                limpar_terminal()
                     
         
-        
-    
+
         
     
 def menu(opc):
+    ''' Um menu que recebe uma opcao e retorna um termo de busca'''
+    
+    # string com opçoes de busca
     tipo = ["titulo", "genero", "ano", "diretor", "tipo"]
     textos = ["BUSCA POR TITULO", "BUSCA POR GENERO", "BUSCA POR ANO", "BUSCA POR DIRETOR", "BUSCA POR TIPO"]
     
+    # verifica se a opcao digitada é um numero
     try:
         opc = int(opc)
     except:
         print("Opcao invalida")
         input()
-        #limpar_terminal()
+        limpar_terminal()
         return
-    if(opc == 0):
+    
+    if(opc == 0): # se o usuario escolher sair o servidor é fechado
         print("Saindo...")
         closeServer()
         exit()
-    elif(opc < 1 or opc > 5):
+    elif(opc < 1 or opc > 5): # se o usuario escolher uma opcao invalida
         print("Opcao invalida")
         input()
-        #limpar_terminal()
+        limpar_terminal()
         return
-    else:
-        #limpar_terminal()
+    else: # se o usuario escolher uma opcao valida
+        # impressao de cabeçalho de acordo com a opcao escolhida
+        limpar_terminal()
         logo_pequena()
         print(textos[opc-1])
+        
+        # caso a opcao seja buscar por tipo o usuario deve escolher entre filme e serie
         if(opc == 5):
             print("1. Filme")
             print("2. Serie")
+            
+            # verifica se a opcao digitada é um numero
             try:
                 opc = int(input("Digite o tipo: "))
             except:
@@ -108,6 +146,7 @@ def menu(opc):
                 input()
                 #limpar_terminal()
                 return
+            # Atribui o tipo de busca de acordo com a opcao escolhida
             if opc == 1:
                 busca = "filme"
             elif opc == 2:
@@ -115,13 +154,14 @@ def menu(opc):
             else:
                 print("Opcao invalida")
                 input()
-                #limpar_terminal()
+                limpar_terminal()
                 return
-        else:
+        else: # se a opcao nao for buscar por tipo o usuario deve digitar o termo de busca
             busca = input(f"Digite o {tipo[opc-1]}: ")
         return busca
 
 def limpar_terminal():
+    ''' Limpa o terminal de acordo com o sistema operacional '''
     sistema_operacional = os.name
 
     if sistema_operacional == 'posix':  # Linux ou macOS
@@ -133,13 +173,15 @@ def limpar_terminal():
 
 
 def print_media(list):
+    ''' Imprime os items da lista recebida e retorna uma lista com os seus ids'''
+    
     ids = []
     for i in list:
         print(f'{i["id"]} - \t{i["title_ptBR"]} ({i["original_title"]})')
-        if i["type"] == "filme":
+        if i["type"] == "filme": # se for um filme
             print("\t   - Filme")
             print(f'\t   - Ano: {i["year"]}\n\t   - Duracao{i["duration"]} min')
-        else:
+        else: # se for uma serie
             print("\t   - Serie")
             print(f'\t   - Lancamento: {i["premiered"].split("-")[0]}\n\t   - {i["seasons"]} temporadas')
         ids.append(str(i["id"]))
