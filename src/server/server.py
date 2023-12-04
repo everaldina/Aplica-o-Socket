794a2e41787408e62488e468d7ddb9dd3d592c8f
Everaldina G. Barbosa <everaldinag@gmail.com>
2023-12-04 19:09:18 -0300

    add comentarios server

--- src/server/server.py
+++ src/server/server.py
@@ -2,23 +2,24 @@ import socket
 from CiCflix import searchFiles, searchFile
 import json
 
-#1: Criar o socket TCP/IP
+#Criar o socket TCP/IP
 host = '127.0.0.1'
 port = 12345
 servidor_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 servidor_socket.bind((host, port))
 
 
-# 2: Escutar
+# Colocar o socket para escutar conexões
 servidor_socket.listen()
 
+# loop enquanto o servidor estiver rodando
 while(1):
-    # 3: Aceitar conexão
+    # Aceitar conexão
     client_connection, client_address = servidor_socket.accept()
     print("Conectado com: ", client_address)
 
 
-    # Recebe stream de dados
+    # Recebe stream de solicitação do cliente
     stream = client_connection.recv(1024).decode().split("---")
     
     if(stream[0] == "search"): # Se for uma solicitação de busca
@@ -48,10 +49,10 @@ while(1):
         # Aguarda confirmação de que os dados foram recebidos
         if(client_connection.recv(1024).decode() == "DONE"):
             print("Dados enviados")
-            print("Fechando conexao com ", client_address)
         else:
             print("Erro ao enviar dados")
 
+        print("Fechando conexao com ", client_address)
         client_connection.close() # fecha a conexão
         
     elif(stream[0] == "stream"): # se for uma solicitaçao de stream
@@ -73,8 +74,15 @@ while(1):
             part = data[i:i+1024]
             client_connection.send(part.encode())
         
+        if(client_connection.recv(1024).decode() == "DONE"):
+            print("Dados enviados")
+        else:
+            print("Erro ao enviar dados")
+        
         # fecha a conexão com o cliente
+        print("Fechando conexao com ", client_address)
         client_connection.close()
+        
     elif(stream[0] == "end"): # se for uma solicitação de encerramento
         print(f"Fechando conexao com {client_address}")
         client_connection.close()
@@ -83,4 +91,5 @@ while(1):
         break
     else: # se for um stream desconhecido
         print("Erro: Comando desconhecido")
+        print("Fechando conexao com ", client_address)
         client_connection.close()
